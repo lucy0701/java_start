58fc08c1e78cb507745865e6b1d4559071fa762c
lucy0701 <sasain71@naver.com>
2023-06-06 21:19:28 +0900

    0606

--- src/com/mycompany/study/ch05/ExArrayInArray.java
+++ src/com/mycompany/study/ch05/ExArrayInArray.java
@@ -1,19 +1,41 @@
 package com.mycompany.study.ch05;
 
 public class ExArrayInArray {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
+        int[][] score = {{50, 10, 30}, {10, 30, 30}, {10, 60, 30}, {30, 80, 30}, {90, 100, 30}};
+        String[] name = {"도형", "수철", "민지", "민아", "장철", "진서"};
 
-		int[][] score = {{99,88,44}, {55,77,66}, {55,55,44}, {55,55,44}, {55,55,77}};
-		
-		int sum = 0;
-		for(int i= 0; i < score.length; i++) {
-			for(int j=0; j < score[i].length; j++) {
-				System.out.println("score[" + i + "][" + j + "] : " + score[i][j]);
-			}
-			System.out.println("----------");
-		}
+        int[] subjectTotal = new int[score[0].length];
+        int[] personTotal = new int[score.length];
 
+        for (int i = 0; i < score.length; i++) {
+            for (int j = 0; j < score[i].length; j++) {
+                subjectTotal[j] += score[i][j];
+                personTotal[i] += score[i][j];
+            }
+        }
 
-	}
+        System.out.println("------------과목별 총합--------------");
+        for (int i = 0; i < subjectTotal.length; i++) {
+            System.out.println(name[i] + "의 총합 : " + subjectTotal[i] + "입니다.");
+        }
+
+        System.out.println("------------과목별 평균--------------");
+        for (int i = 0; i < subjectTotal.length; i++) {
+            double average = (double) subjectTotal[i] / score.length;
+            System.out.println(name[i] + "의 평균 : " + average + "입니다.");
+        }
+
+        System.out.println("------------사람별 총합--------------");
+        for (int i = 0; i < personTotal.length; i++) {
+            System.out.println(name[i] + "의 총합 : " + personTotal[i] + "입니다.");
+        }
+
+        System.out.println("------------사람별 평균--------------");
+        for (int i = 0; i < personTotal.length; i++) {
+            double average = (double) personTotal[i] / score[i].length;
+            System.out.println(name[i] + "의 평균 : " + average + "입니다.");
+        }
+    }
 }
